b69e762e881e548c5f236152c35fdad8c02b4898
Ivan8or <ivanthegreat825@gmail.com>
2023-05-08 04:33:14 -0400

    added unit test for flipping sides

---
+++ src/test/java/tests/board/LSideTest.java
@@ -0,0 +1,14 @@
+package tests.board;
+
+import chess.board.LSide;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LSideTest {
+
+    @Test
+    public void flip() {
+        Assert.assertEquals(LSide.BLACK.flip(), LSide.WHITE);
+        Assert.assertEquals(LSide.WHITE.flip(), LSide.BLACK);
+    }
+}
