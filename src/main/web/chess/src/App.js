cf59e5963a323e6486f2da7772191c94a88bed30
Ivan8or <ivanthegreat825@gmail.com>
2023-05-09 03:34:51 -0400

    refactored to use one root component

--- src/main/web/chess/src/App.js
+++ src/main/web/chess/src/App.js
@@ -1,15 +1,24 @@
 import React, {useState} from 'react';
 import ChessBoard from './chessboard/ChessBoard'
+import Menu from './menu/Menu';
 
 export default function App() {
 
-    
     const initialFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 
     const [fenState, updateFenState] = useState(initialFen);
 
     return (
-        <ChessBoard fen={fenState}/>
+        <>
+        <div id="board-root">
+            <ChessBoard  fen={fenState} />
+        </div>
+
+        <div id="menu-root">
+            <Menu  />
+        </div>
+        
+        </>
     );
 }
 
--- src/main/web/chess/src/index.js
+++ src/main/web/chess/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import Menu from './menu/Menu';
 
-const boardRootElement = document.getElementById('board-root');
+const boardRootElement = document.getElementById('root');
 const boardRoot = createRoot(boardRootElement);
 boardRoot.render(<App/>);
-
-
-const menuRootElement = document.getElementById('menu-root');
-const menuRoot = createRoot(menuRootElement);
-menuRoot.render(<Menu/>);
