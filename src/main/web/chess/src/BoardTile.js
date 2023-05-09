0f8ba6b3efc115a9f8f581931de6faf8536e3dd9
Ivan8or <ivanthegreat825@gmail.com>
2023-05-08 20:46:12 -0400

    added splitting fen into tiles

---
+++ src/main/web/chess/src/BoardTile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function BoardTile( {tile} ) {
+    
+    return (
+        <div> {tile} </div>
+    );
+}
---
+++ src/main/web/chess/src/ChessBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BoardTile from './BoardTile';
+
+
+export default function ChessBoard( {fen} ) {
+
+    const tiles = [...fen]
+        .filter((c) => c !== '/')
+        .map((c) => parseInt(c) ? '.'.repeat(parseInt(c)) : c)
+        .join('');
+
+    return (
+        [...tiles].map(t => <BoardTile key={t} tile={t} />)
+    );
+}
