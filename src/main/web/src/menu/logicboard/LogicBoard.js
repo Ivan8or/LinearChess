ebcbbef7953c1d5173ea06d5cbc352c090952c29
Ivan8or <ivanthegreat825@gmail.com>
2023-05-17 10:12:06 -0400

    added basic draggability to tiles

--- src/main/web/src/menu/logicboard/LogicBoard.js
+++ src/main/web/src/menu/logicboard/LogicBoard.js
@@ -3,7 +3,7 @@ import LogicTile from './LogicTile';
 
 export default function DragBoard () {
     
-    const testTiles = [{x: 10, y: 10, width: 40, height: 20}];
+    const testTiles = [{x: 10, y: 10, width: 40, height: 20}, {x: 100, y: 100, width: 40, height: 20}];
     const [tilesState, setTilesState] = useState(testTiles);
 
     const tileComponents = tilesState.map((t, i) => <LogicTile key={i} tile={t} />);
--- src/main/web/src/menu/logicboard/LogicTile.js
+++ src/main/web/src/menu/logicboard/LogicTile.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 
 export default function LogicTile( {tile} ) {
     
     return (
-        <div className="logic-tile" draggable="true"
-        style={{
-            width: tile.width + '%',
-            height: tile.height + '%',
-            left: tile.x + '%',
-            top: tile.y + '%'
-        }}
-        ondrag={onDragTile}
-        ></div>
+
+        <Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[1,1]}
+        scale={1}>
+            <div className="logic-tile handle"
+            style={{
+                width: tile.width + '%',
+                height: tile.height + '%',
+                left: tile.x + '%',
+                top: tile.y + '%'
+            }}>AAA</div>
+        </Draggable>
+      
+        
     );
 }
 
