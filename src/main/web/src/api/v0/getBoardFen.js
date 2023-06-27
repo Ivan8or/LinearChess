3a1e4f670cac62d3a970e6830a303ca00ac6350d
Ivan8or <ivanthegreat825@gmail.com>
2023-06-27 08:28:00 -0400

    dummy api calls will never be allowed in production

--- src/main/web/src/api/v0/getBoardFen.js
+++ src/main/web/src/api/v0/getBoardFen.js
@@ -1,7 +1,7 @@
 const API_ENDPOINT = "/api/v0/fen";
 
 export default function getBoardFen() {
-    if(process.env.REACT_APP_DUMMY_API_CALLS === "true")
+    if(process.env.NODE_ENV !== 'production' && process.env.REACT_APP_DUMMY_API_CALLS === "true")
         return new Promise((res, err) => res("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
 
     return fetch(process.env.REACT_APP_API_URL + API_ENDPOINT, {method: "GET"}).then((response) => response.text());
--- src/main/web/src/api/v0/setLogicBoard.js
+++ src/main/web/src/api/v0/setLogicBoard.js
@@ -2,10 +2,8 @@ const API_ENDPOINT = "/api/v0/logicboard";
 const URL = process.env.REACT_APP_API_URL + API_ENDPOINT;
 
 export default function setLogicBoard(logicboard) {
-    if(process.env.REACT_APP_DUMMY_API_CALLS === "true") {
-        console.log("accessed",API_ENDPOINT,"endpoint");
+    if(process.env.NODE_ENV !== 'production' && process.env.REACT_APP_DUMMY_API_CALLS === "true")
         return new Promise((res, err) => res( {success: "true"} ));
-    }
 
     return fetch(URL, 
             {
