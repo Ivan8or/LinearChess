4c3c332aa818874588159d15efe78a57a2e5b058
Ivan8or <ivanthegreat825@gmail.com>
2023-06-28 21:50:04 -0400

    refactored v0 api instantiation away from Main

--- backend/src/main/java/api/Main.java
+++ backend/src/main/java/api/Main.java
@@ -1,5 +1,6 @@
 package api;
 
+import api.v0.APIv0;
 import api.v0.impl.*;
 import chess.ChessGame;
 import chess.agent.impl.AlphaBetaAgent;
@@ -14,78 +15,7 @@ import java.util.Random;
 import static spark.Spark.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        ChessGame game = new ChessGame();
-        game.setWhiteAgent(simpleAgent(LSide.WHITE));
-        game.setBlackAgent(simpleAgent(LSide.BLACK));
-        registerV0(game);
-    }
-
-    public static void registerV0(ChessGame game) {
-
-        String pathV0 = "/api/v0";
-
-        LChessAPI apiV0 = new LChessAPI()
-                .withPort(3100)
-                .withTimeout(8000)
-                .withCommonPath("/api/v0")
-                .withEndpoint(new GetBoardState(game))
-                .withEndpoint(new RestartGame(game))
-                .withEndpoint(new StepGame(game))
-                .withEndpoint(new GetPossibleMoves(game))
-                .withEndpoint(new MakeMove(game))
-                .withEndpoint(new SetPlayerEval(game))
-                .start();
-    }
-
-
-    public static ChessAgent simpleAgent(LSide side) {
-
-        ChessEval numMyPieces = pieceWeights(side);
-        ChessEval numOpPieces = pieceWeights(side.flip());
-
-        DistanceAcrossEval myProgress = new DistanceAcrossEval(
-                LPieceType.PAWN, side);
-
-        DistanceAcrossEval opProgress = new DistanceAcrossEval(
-                LPieceType.PAWN, side);
-
-        RandomEval rand = new RandomEval(new Random());
-
-        GameEndsEval gameEnds = new GameEndsEval(side);
-        GameDrawsEval gameDraws = new GameDrawsEval();
-
-        CumulativeEval outerEval = new CumulativeEval(
-                new WeightedEval(gameEnds, 1000),
-                new WeightedEval(gameDraws, -100),
-                new WeightedEval(numMyPieces, 10),
-                new WeightedEval(numOpPieces, -10),
-                new WeightedEval(opProgress, -0.5),
-                new WeightedEval(myProgress, 0.5),
-                new WeightedEval(rand, 0.001)
-        );
-        return new AlphaBetaAgent(outerEval, 2);
-    }
-
-    public static ChessEval pieceWeights(LSide side) {
-        NumPiecesEval numPawns = new NumPiecesEval(
-                LPieceType.PAWN, side);
-        NumPiecesEval numRooks = new NumPiecesEval(
-                LPieceType.ROOK, side);
-        NumPiecesEval numKnights = new NumPiecesEval(
-                LPieceType.KNIGHT, side);
-        NumPiecesEval numBishops = new NumPiecesEval(
-                LPieceType.BISHOP, side);
-        NumPiecesEval numQueens = new NumPiecesEval(
-                LPieceType.QUEEN, side);
-
-        return new CumulativeEval(
-                new WeightedEval(numPawns, 1),
-                new WeightedEval(numRooks, 5),
-                new WeightedEval(numKnights, 3),
-                new WeightedEval(numBishops, 3),
-                new WeightedEval(numQueens, 9)
-        );
+        APIv0.registerV0();
     }
 }
---
+++ backend/src/main/java/api/v0/APIv0.java
@@ -0,0 +1,87 @@
+package api.v0;
+
+import api.LChessAPI;
+import api.v0.impl.*;
+import chess.ChessGame;
+import chess.agent.ChessAgent;
+import chess.agent.impl.AlphaBetaAgent;
+import chess.board.LPieceType;
+import chess.board.LSide;
+import chess.eval.ChessEval;
+import chess.eval.impl.*;
+
+import java.util.Random;
+
+public class APIv0 {
+
+    final static public String rootPath = "/api/v0";
+
+    public static void registerV0() {
+
+        ChessGame game = new ChessGame();
+        game.setWhiteAgent(simpleAgent(LSide.WHITE));
+        game.setBlackAgent(simpleAgent(LSide.BLACK));
+
+
+        new LChessAPI()
+                .withPort(3100)
+                .withTimeout(8000)
+                .withCommonPath(rootPath)
+                .withEndpoint(new GetBoardState(game))
+                .withEndpoint(new RestartGame(game))
+                .withEndpoint(new StepGame(game))
+                .withEndpoint(new GetPossibleMoves(game))
+                .withEndpoint(new MakeMove(game))
+                .withEndpoint(new SetPlayerEval(game))
+                .start();
+    }
+
+    public static ChessAgent simpleAgent(LSide side) {
+
+        ChessEval numMyPieces = pieceWeights(side);
+        ChessEval numOpPieces = pieceWeights(side.flip());
+
+        DistanceAcrossEval myProgress = new DistanceAcrossEval(
+                LPieceType.PAWN, side);
+
+        DistanceAcrossEval opProgress = new DistanceAcrossEval(
+                LPieceType.PAWN, side);
+
+        RandomEval rand = new RandomEval(new Random());
+
+        GameEndsEval gameEnds = new GameEndsEval(side);
+        GameDrawsEval gameDraws = new GameDrawsEval();
+
+        CumulativeEval outerEval = new CumulativeEval(
+                new WeightedEval(gameEnds, 1000),
+                new WeightedEval(gameDraws, -100),
+                new WeightedEval(numMyPieces, 10),
+                new WeightedEval(numOpPieces, -10),
+                new WeightedEval(opProgress, -0.5),
+                new WeightedEval(myProgress, 0.5),
+                new WeightedEval(rand, 0.001)
+        );
+        return new AlphaBetaAgent(outerEval, 2);
+    }
+
+    public static ChessEval pieceWeights(LSide side) {
+        NumPiecesEval numPawns = new NumPiecesEval(
+                LPieceType.PAWN, side);
+        NumPiecesEval numRooks = new NumPiecesEval(
+                LPieceType.ROOK, side);
+        NumPiecesEval numKnights = new NumPiecesEval(
+                LPieceType.KNIGHT, side);
+        NumPiecesEval numBishops = new NumPiecesEval(
+                LPieceType.BISHOP, side);
+        NumPiecesEval numQueens = new NumPiecesEval(
+                LPieceType.QUEEN, side);
+
+        return new CumulativeEval(
+                new WeightedEval(numPawns, 1),
+                new WeightedEval(numRooks, 5),
+                new WeightedEval(numKnights, 3),
+                new WeightedEval(numBishops, 3),
+                new WeightedEval(numQueens, 9)
+        );
+    }
+}
