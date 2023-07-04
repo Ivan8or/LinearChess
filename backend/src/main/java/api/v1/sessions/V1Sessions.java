b76a415c2efb8642ff0e03d45d9d5afb1925e48e
Ivan8or <ivanthegreat825@gmail.com>
2023-07-04 08:28:12 -0400

    added get session endpoint for validation / rediscovering lobbies

--- backend/src/main/java/api/v1/sessions/V1Sessions.java
+++ backend/src/main/java/api/v1/sessions/V1Sessions.java
@@ -3,24 +3,41 @@ package api.v1.sessions;
 import api.util.APIEndpoint;
 import api.util.validator.SessionValidator;
 import model.api.Model;
+import model.lobby.ChessLobby;
 import model.mappings.ApiResponse;
 import model.mappings.Session;
 import spark.Request;
 import spark.Response;
 import util.JsonConverter;
 
+import java.util.Map;
 import java.util.Optional;
 
-import static spark.route.HttpMethod.delete;
-import static spark.route.HttpMethod.post;
+import static spark.route.HttpMethod.*;
 
 public class V1Sessions extends APIEndpoint {
 
     final private Model model;
 
     public V1Sessions(Model model) {
-        super("/api/v1/sessions", post, delete);
+        super("/api/v1/sessions", get, post, delete);
         this.model = model;
+    }
+
+    protected Object get(Request request, Response response) {
+        String sessionJson = request.headers("session");
+        Optional<Session> session = JsonConverter.fromJson(sessionJson, Session.class);
+
+        Optional<ApiResponse> invalid = SessionValidator.validate(session, model);
+        if(invalid.isPresent())
+            return invalid.get();
+
+        Optional<ChessLobby> lobby = model.getSessions().sessionLobby(session.get());
+
+        if(lobby.isEmpty())
+            return new ApiResponse(200, "VALID_SESSION");
+
+        return Map.of("status", 200, "message","VALID_SESSION","lobby", lobby.get().getLobbyId());
     }
 
     protected Object post(Request request, Response response) {
@@ -35,12 +52,6 @@ public class V1Sessions extends APIEndpoint {
         Optional<ApiResponse> invalid = SessionValidator.validate(session, model);
         if(invalid.isPresent())
             return invalid.get();
-
-        if(sessionJson == null || session.isEmpty())
-            return new ApiResponse(401, "NO_SESSION_HEADER");
-
-        if(!model.getSessions().validSession(session.get()))
-            return new ApiResponse(403, "BAD_SESSION_HEADER");
 
         model.getSessions().endSession(session.get());
         return new ApiResponse(200, "SUCCESS");
